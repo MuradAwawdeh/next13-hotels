235b9709a07c164f785690fee198abd59ecadd3a
Murad Awawdeh <murad@safarway.com>
2023-03-03 00:07:19 +0200

    ...

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 export default function Home() {
   return (
-    <div>
+    <div style={styles}>
         <h1><span style={{color: 'blue', fontWeight: 'bold'}}>Murad</span> Hotels Booking</h1>
-        <div>100% <b>Fakher</b> free </div>
         <div> 100% <b>Khamed</b> free</div>
+        <div>100% <b>Fakher</b> free </div>
         <h2>comming SooooooooooooooooooooooooooooN!</h2>
     </div>
   )
+}
+
+const styles = {
+  maxWidth: '600px',
+  margin: '0 auto',
+  boxShadow: '0 13px 13px #00000033',
+  padding: '20px',
+  borderRadius: '10px',
+  marginTop: '50px'
 }
